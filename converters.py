c22bd6f84eb1d40e9dfa9b4404b6f0cd8bd8dba2
Matt-Conrad <mattgrayconrad@gmail.com>
2020-02-25 14:28:54 -0500

    np_arr_to_pixmap now assumes the float64 arrays have an intensity range of 0-1

--- converters.py
+++ converters.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PyQt5.QtGui import QPixmap, QImage
 
-def np_arr_to_pixmap(image, maximum_intensity=None):
+def np_arr_to_pixmap(image):
     # Scale the pixel intensity to uint8
     if image.dtype == 'bool':
         rescaled_image = (image.astype(np.uint8) * 255)
     elif image.dtype == 'float64':
-        rescaled_image = (image/maximum_intensity * 255).astype(np.uint8)
+        rescaled_image = (image * 255).astype(np.uint8)
     rescaled_image = np.ascontiguousarray(rescaled_image)
     # Create the QPixmap object
     height, width = rescaled_image.shape
